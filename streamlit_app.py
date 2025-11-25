6dcdc9c9e0b9598e2569e60bc205b63b5200d037
namgoonghyuk <H098S02306@chunchon.gwe.hs.kr>
2025-11-25 12:12:08 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,6 +77,8 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
      labe{1} : {"texts": [탕수육],"images": ["https://static.hubzum.zumst.com/hubzum/2018/09/04/13/868a77c37e374f1fa5e93c766b70fcf9_780x0c.jpg"]},
      labe{2} : {"texts": [중국식 냉면],"images": ["https://data.ygosu.com/upload_files/board_food/77263/60eb63ad53e6a.jpg"]},
      labe{3} : {"texts": [짜장면],"images": ["https://img.siksinhot.com/article/1614912249689885.jpg"]},
+
+}
 # ======================
 def load_pil_from_bytes(b: bytes) -> Image.Image:
     pil = Image.open(BytesIO(b))
